c0f15e8c7d23dad34a7e19737b25029a43c36ec2
Engin AYDOGAN <engin@bzzzt.biz>
2012-12-23 22:36:35 +0200

    Preliminary XML output support

--- Zita/Filters/AutoFormatFilter.php
+++ Zita/Filters/AutoFormatFilter.php
@@ -5,6 +5,38 @@ use Zita\Request;
 use Zita\Response;
 use Zita\Dispatcher;
 use Zita\Service;
+/*
+ * This assocToXML implementation is borrowed from  James Earlywine - July 20th 2011
+ *
+ * And modified. I've added $root support and turned PHP_EOLs to "\n" because an interface must be consistent no matter
+ * which platform it is deployed on.
+ */
+function assocToXML ($theArray, $tabCount=2, $root = true)
+{
+    $tabCount++;
+    $tabSpace = "";
+    $extraTabSpace = "";
+    for ($i = 0; $i<$tabCount; $i++) {
+        $tabSpace .= " ";
+    }
+
+    if($root)
+        $theXML = '<root>';
+    // parse the array for data and output xml
+    foreach($theArray as $tag => $val) {
+        if (!is_array($val)) {
+            $theXML .= "\n".$tabSpace.'<'.$tag.'>'.htmlentities($val).'</'.$tag.'>';
+        } else {
+            $tabCount++;
+            $theXML .= "\n".$tabSpace.'<'.$tag.'>'.assocToXML($val, $tabCount, false);
+            $theXML .= "\n".$tabSpace.'</'.$tag.'>';
+        }
+    }
+    if($root)
+        $theXML .= "\n</root>";
+
+    return $theXML;
+}
 
 class AutoFormatFilter extends \Zita\OutputFilter
 {
@@ -22,10 +54,13 @@ class AutoFormatFilter extends \Zita\OutputFilter
                 if($req->params->callback != null)
                     $resp->body = $req->params->callback.'('.$resp->body.');';
                 break;
+            case 'xml';
+                $resp->body = assocToXML($resp->body);
+                break;
             case 'raw':
                 break;
             default:
-                trigger_error('Unknown format.');
+                throw new \Zita\DispatcherException("@OutputFilter AutoFormat: Unknown format $type, please fix the 'type' parameter in the Request.");
         }
     }
 
